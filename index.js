f30486481e4837d6a2ae5adce5f78353af4c5856
Sanket Patil <patilsanket209@gmail.com>
2022-03-31 21:54:47 +0530

    completed backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connect = require("./src/configs/db")
+const app = express();
+const kidController= require("./src/controllers/kid.controller")
+
+app.use(express.json())
+app.use("/kids", kidController)
+app.listen(1296,async()=>{
+    try{
+        await connect();
+    }catch(err){
+        console.log(err.message)
+    }
+    console.log("Listening to port 1296");
+})
