01c8172be5ab797d0793918d2edf82479c6e2aba
Sanket Patil <patilsanket209@gmail.com>
2022-04-01 12:11:41 +0530

    added cors files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const connect = require("./configs/db")
+const cors = require('cors');
 const app = express();
 const kidController= require("./controllers/kid.controller")
 require("dotenv").config()
 const port =process.env.PORT||1296
 app.use(express.json())
+app.use(cors());
 app.use("/kids", kidController)
 
 app.listen( port,async()=>{
